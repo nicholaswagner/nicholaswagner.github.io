3f380961326df066a802654cfca976a99934b7de
nicholaswagner <nicholas@secretsignal.com>
2020-04-09 13:43:17 -0500

    adds a goodenough-tm version of site

---
+++ precache-manifest.d157fbfbcf2b4b2dec8fdb50870d348d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "818642b0cebf8f808d59bdc6e9f446df",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fc7560252044cfb3c818",
+    "url": "/static/css/main.8b13074d.chunk.css"
+  },
+  {
+    "revision": "e906beba6d5a33b5e9a5",
+    "url": "/static/js/2.03f3faac.chunk.js"
+  },
+  {
+    "revision": "fc7560252044cfb3c818",
+    "url": "/static/js/main.a1919f9f.chunk.js"
+  },
+  {
+    "revision": "8f5fde1b56386be72ab4",
+    "url": "/static/js/runtime-main.cfdd6197.js"
+  },
+  {
+    "revision": "ee5b9e95d90f6edad4233ec87a011a6c",
+    "url": "/static/media/square_portrait.ee5b9e95.jpg"
+  }
+]);
